nagyist/neoscms-neos-ui

commit 36a5de71e2f1d9da88d64bb8ede316bcd47083fe

TASK: Inline `ConflictsBuilder` to better document deduplication logic

Classes/Infrastructure/ContentRepository/ConflictsFactory.php
@@ -44,6 +44,7 @@ use Neos\ContentRepository\Core\SharedModel\Node\NodeAggregateId;
 use Neos\ContentRepository\Core\SharedModel\Workspace\Workspace;
 use Neos\ContentRepository\Core\SharedModel\Workspace\WorkspaceName;
 use Neos\Flow\Annotations as Flow;
+use Neos\Flow\Utility\Algorithms;
 use Neos\Neos\Domain\NodeLabel\NodeLabelGeneratorInterface;
 use Neos\Neos\Domain\Service\NodeTypeNameFactory;
 use Neos\Neos\Ui\Application\Shared\Conflict;
@@ -76,14 +77,18 @@ final class ConflictsFactory
     public function fromWorkspaceRebaseFailed(
         WorkspaceRebaseFailed $workspaceRebaseFailed
     ): Conflicts {
-        $conflictsBuilder = Conflicts::builder();
+        /** @var array<string,Conflict> */
+        $conflictsByKey = [];
 
         foreach ($workspaceRebaseFailed->commandsThatFailedDuringRebase as $commandThatFailedDuringRebase) {
             $conflict = $this->createConflictFromCommandThatFailedDuringRebase($commandThatFailedDuringRebase);
-            $conflictsBuilder->addConflict($conflict);
+            if (array_key_exists($conflict->key, $conflictsByKey)) {
+                // deduplicate if the conflict affects the same node
+                $conflictsByKey[$conflict->key] = $conflict;
+            }
         }
 
-        return $conflictsBuilder->build();
+        return new Conflicts(...$conflictsByKey);
     }
 
     private function createConflictFromCommandThatFailedDuringRebase(

Classes/Application/Shared/Conflicts.php
@@ -27,12 +27,7 @@ final readonly class Conflicts implements \JsonSerializable, \Countable
 
     public function __construct(Conflict ...$items)
     {
-        $this->items = $items;
-    }
-
-    public static function builder(): ConflictsBuilder
-    {
-        return new ConflictsBuilder();
+        $this->items = array_values($items);
     }
 
     public function jsonSerialize(): mixed

Classes/Application/Shared/ConflictsBuilder.php
@@ -1,49 +1,0 @@
-<?php
-
-/*
- * This file is part of the Neos.Neos.Ui package.
- *
- * (c) Contributors of the Neos Project - www.neos.io
- *
- * This package is Open Source Software. For the full copyright and license
- * information, please view the LICENSE file which was distributed with this
- * source code.
- */
-
-declare(strict_types=1);
-
-namespace Neos\Neos\Ui\Application\Shared;
-
-use Neos\Flow\Annotations as Flow;
-
-/**
- * @internal
- */
-#[Flow\Proxy(false)]
-final class ConflictsBuilder
-{
-    /**
-     * @var Conflict[]
-     */
-    private array $items = [];
-
-    /**
-     * @var array<string,Conflict>
-     */
-    private array $itemsByKey = [];
-
-    public function addConflict(Conflict $conflict): self
-    {
-        if (!isset($this->itemsByKey[$conflict->key])) {
-            $this->itemsByKey[$conflict->key] = $conflict;
-            $this->items[] = $conflict;
-        }
-
-        return $this;
-    }
-
-    public function build(): Conflicts
-    {
-        return new Conflicts(...$this->items);
-    }
-}

commit 829645a1555bcbf85e33a4cf23239d8fac9fa655

BUGFIX: No conflicts are shown
Regression from 36a5de71e2f1d9da88d64bb8ede316bcd47083fe as the deduplication did not work.

Classes/Infrastructure/ContentRepository/ConflictsFactory.php
@@ -76,9 +76,9 @@ final class ConflictsFactory
         /** @var array<string,Conflict> */
         $conflictsByKey = [];
 
-        foreach ($workspaceRebaseFailed->conflictingEvents as $eventThatFailedDuringRebase) {
-            $conflict = $this->createConflictFromEventThatFailedDuringRebase($eventThatFailedDuringRebase);
-            if (array_key_exists($conflict->key, $conflictsByKey)) {
+        foreach ($workspaceRebaseFailed->conflictingEvents as $conflictingEvent) {
+            $conflict = $this->createConflict($conflictingEvent);
+            if (!array_key_exists($conflict->key, $conflictsByKey)) {
                 // deduplicate if the conflict affects the same node
                 $conflictsByKey[$conflict->key] = $conflict;
             }