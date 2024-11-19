c964e09e095b45f198b6b60c4b65e8c3e88ce90f
mhsdesign <85400359+mhsdesign@users.noreply.github.com>
2024-11-19 14:07:29 +0100

    TASK: Adjust to soft removal of `CopyNodesRecursively`

--- Classes/Domain/NodeCreation/NodeCreationCommands.php
+++ Classes/Domain/NodeCreation/NodeCreationCommands.php
@@ -17,7 +17,6 @@ use Neos\ContentRepository\Core\Feature\NodeCreation\Command\CreateNodeAggregate
 use Neos\ContentRepository\Core\Feature\NodeCreation\Dto\NodeAggregateIdsByNodePaths;
 use Neos\ContentRepository\Core\Feature\NodeDisabling\Command\DisableNodeAggregate;
 use Neos\ContentRepository\Core\Feature\NodeDisabling\Command\EnableNodeAggregate;
-use Neos\ContentRepository\Core\Feature\NodeDuplication\Command\CopyNodesRecursively;
 use Neos\ContentRepository\Core\Feature\NodeModification\Command\SetNodeProperties;
 use Neos\ContentRepository\Core\Feature\NodeModification\Dto\PropertyValuesToWrite;
 use Neos\ContentRepository\Core\Feature\NodeReferencing\Command\SetNodeReferences;
@@ -59,13 +58,13 @@ final readonly class NodeCreationCommands implements \IteratorAggregate
      * Add a list of commands that are executed after the initial created command was run.
      * This allows to create child-nodes and append other allowed commands.
      *
-     * @var array<int,CreateNodeAggregateWithNode|SetNodeProperties|DisableNodeAggregate|EnableNodeAggregate|SetNodeReferences|CopyNodesRecursively>
+     * @var array<int,CreateNodeAggregateWithNode|SetNodeProperties|DisableNodeAggregate|EnableNodeAggregate|SetNodeReferences>
      */
     public array $additionalCommands;
 
     private function __construct(
         CreateNodeAggregateWithNode $first,
-        CreateNodeAggregateWithNode|SetNodeProperties|DisableNodeAggregate|EnableNodeAggregate|SetNodeReferences|CopyNodesRecursively ...$additionalCommands
+        CreateNodeAggregateWithNode|SetNodeProperties|DisableNodeAggregate|EnableNodeAggregate|SetNodeReferences ...$additionalCommands
     ) {
         $this->first = $first;
         $this->additionalCommands = array_values($additionalCommands);
@@ -117,7 +116,7 @@ final readonly class NodeCreationCommands implements \IteratorAggregate
     }
 
     public function withAdditionalCommands(
-        CreateNodeAggregateWithNode|SetNodeProperties|DisableNodeAggregate|EnableNodeAggregate|SetNodeReferences|CopyNodesRecursively ...$additionalCommands
+        CreateNodeAggregateWithNode|SetNodeProperties|DisableNodeAggregate|EnableNodeAggregate|SetNodeReferences ...$additionalCommands
     ): self {
         return new self($this->first, ...$this->additionalCommands, ...$additionalCommands);
     }
